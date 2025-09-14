1309932e15c68812850e03deb30c952a042f4a64
yenulisiri <yenulisiri@gmail.com>
2025-09-14 07:13:49 +0530

    Update models.py

--- testapp/models.py
+++ testapp/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Todo(models.Model):
     title = models.CharField(max_length=200)
-    is_completed = models.BooleanField(default=False)
+    is_completed = models.BooleanField(default=false)
 
     def __str__(self):
         return self.title
